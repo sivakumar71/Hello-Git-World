5d4dbf0912ef74f714f7cb93841e40aec6631fbb
sivakumar71 <47490479+sivakumar71@users.noreply.github.com>
2019-02-10 09:46:15 -0600

    Hello World with Lambda!
    Converted the print functionality to lambda

--- HelloGitWorld.cpp
+++ HelloGitWorld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Hello World with input args
+// Hello World with input argsa
 int main(int argc, char** argv)
 {
 	string inputArgs[argc];
@@ -9,7 +9,15 @@ int main(int argc, char** argv)
 	for (int i = 0; i < argc; i++)
 		inputArgs[i] = string(argv[i]);
 		
-	for ( auto str : inputArgs)
-		cout << "Hello, " << str << " Gitword!" << endl;
+	// Define a lambda function that prints inputArgs
+	// The [&] capture clause allows the lambda to access outside variables by reference
+	auto HelloFunc = [&] () {
+		for ( auto str : inputArgs)
+			cout << "Hello, " << str << " Gitworld!" << endl;
+	};
+	
+	// Invoke the Lambda 
+	HelloFunc();
+	
 	return 0;
 }
